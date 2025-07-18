7038fd9b3dbe3b63fb032e10f3d767e894fffc30
andrew cooke <andrew@acooke.org>
2025-07-18 15:02:04 -0400

    adc working too

--- Demonstrations+HelloWorlds/PicoSDK/ComputerCard/examples/diagnostics/main.cpp
+++ Demonstrations+HelloWorlds/PicoSDK/ComputerCard/examples/diagnostics/main.cpp
@@ -66,29 +66,42 @@ private:
     }
   }
 
+  // detect and display changes, or identify source of previous change
+
   constexpr static uint NONE = 999;
-  constexpr static uint NOISE = 2;
   uint prev_change = NONE;
   int32_t recent = 0;
+  constexpr static uint NOISE_KNOBS = 3;
   constexpr static uint n_knobs = 3;
   uint32_t knobs[2][n_knobs] = {};
+  constexpr static uint NOISE_ADC = 6;
+  constexpr static uint n_adc = 4;
+  uint32_t adcs[2][n_adc] = {};
+  constexpr static uint n_all = n_knobs + n_adc;
 
   void save_current() {
     for (uint i = 0; i < n_knobs; i++) {
       knobs[1][i] = knobs[0][i];
-      knobs[0][i] = KnobVal(static_cast<Knob>(i)) >> NOISE;
+      knobs[0][i] = KnobVal(static_cast<Knob>(i)) >> NOISE_KNOBS;
+    }
+    for (uint i = 0; i < n_adc; i++) {
+      adcs[1][i] = adcs[0][i];
+      adcs[0][i] = (i < 2 ? AudioIn(i) : CVIn(i-2)) >> NOISE_ADC;
     }
   }
 
   bool changed(uint idx) {
     if (idx < n_knobs) return knobs[0][idx] != knobs[1][idx];
+    idx -= n_knobs;
+    if (idx < n_adc) return adcs[0][idx] != adcs[1][idx];
+    idx -= n_adc;
     return false;
   }
 
   uint next_change(uint old_idx) {
     uint new_idx = old_idx == NONE ? 0 : old_idx + 1;
-    for (uint i = 0; i < n_knobs; i++) {
-      if (new_idx == n_knobs) new_idx = 0;
+    for (uint i = 0; i < n_all; i++) {
+      if (new_idx == n_all) new_idx = 0;
       if (changed(new_idx)) return new_idx;
       new_idx++;
     }
@@ -97,16 +110,23 @@ private:
 
   void identify(uint idx) {
     for (uint i = 0; i < num_leds; i++) LedBrightness(i, 1024);
-    switch (idx) {
-    case static_cast<uint>(Main):
-      for (uint i = 0; i < 4; i++) LedOn(i);
-      return;
-    case static_cast<uint>(X):
-      LedOn(2);
-      LedOn(4);
-      return;
-    case static_cast<uint>(Y):
-      for (uint i = 2; i < 6; i++) LedOn(i);
+    if (idx < n_knobs) {
+      switch(idx) {
+      case static_cast<uint>(Main):
+        for (uint i = 0; i < 4; i++) LedOn(i);
+        return;
+      case static_cast<uint>(X):
+        LedOn(2);
+        LedOn(4);
+        return;
+      case static_cast<uint>(Y):
+        for (uint i = 2; i < 6; i++) LedOn(i);
+        return;
+      }
+    }
+    idx -= n_knobs;
+    if (idx < n_adc) {
+      LedOn(idx);  // swap audio l/r
       return;
     }
   }
@@ -114,6 +134,11 @@ private:
   void display(uint idx) {
     if (idx < n_knobs) {
       columns12bits(KnobVal(static_cast<Knob>(idx)));
+      return;
+    }
+    idx -= n_knobs;
+    if (idx < n_adc) {
+      columns12bits(static_cast<int32_t>(idx < 2 ? AudioIn(idx) : CVIn(idx - 2)));
     }
     return;
   }
